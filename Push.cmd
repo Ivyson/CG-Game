e097b4d91ab005f70ee40fb177d8d9c0b42cb321
82Sam <53128739+Ivyson@users.noreply.github.com>
2024-04-28 17:23:58 +0200

    Auto-Push

--- Push.cmd
+++ Push.cmd
@@ -1,6 +1,6 @@
 @echo off
-git status
-git add . 
+git status  REM Fetch the status and append it on the terminal to show how many files are modified in prep to push the work
+git add .  
 git commit -m"Auto-Push"
 git push
-git status
+git status Rem Retrieve the current status of the repo or the local file system relative to the repo
