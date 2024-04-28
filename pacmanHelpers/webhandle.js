fe7dcf377951131ce687809981958bba09ff69b3
82Sam <53128739+Ivyson@users.noreply.github.com>
2024-04-28 18:19:12 +0200

    Auto-Push

--- pacmanHelpers/webhandle.js
+++ pacmanHelpers/webhandle.js
@@ -14,14 +14,13 @@ WebGLUtils = (function() {
       var names = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
       var context = null;
       for (var ii = 0; ii < names.length; ++ii) {
+        try {
+            context = canvas.getContext(names[ii], opt_attribs);
+          } catch(e) {}
         if (context) {
           break;
         }
-        else{
-            try {
-                context = canvas.getContext(names[ii], opt_attribs);
-              } catch(e) {}
-        }
+
       }
       return context;
     }
@@ -41,4 +40,3 @@ WebGLUtils = (function() {
              window.setTimeout(callback, 1000/60);
            };
   })();
-  
